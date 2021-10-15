20249d65b4a26d18dc800191810e09b27eef58d3
acen20 <ahsen1330@gmail.com>
2021-10-15 21:50:34 +0500

    charts done

--- js/transactions.js
+++ js/transactions.js
@@ -56,6 +56,40 @@ function create_table2_layout() {
   document.querySelector("#table2-container").innerHTML = table_layout;
 }
 
+function create_table3_layout() {
+  var table_layout = `  <table id="dom_jq_event3" class="w-100 table table-striped table-bordered no-wrap">
+      <thead>
+          <tr>
+          <th>Date</th>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+          <th>Ticker</th>
+          <th>Fixation Data</th>
+          <th>Source Tax</th>
+          <th>Source Tax Currency</th>
+          <th>Comment</th>
+          </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      <tfoot>
+          <tr>
+          <th>Date</th>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+          <th>Ticker</th>
+          <th>Fixation Data</th>
+          <th>Source Tax</th>
+          <th>Source Tax Currency</th>
+          <th>Comment</th>
+          </tr>
+      </tfoot>
+  </table>`;
+  document.querySelector("#table3-container").innerHTML = table_layout;
+}
+
 const initialize_table1 = () => {
   var _table = $("#dom_jq_event1").DataTable({
     columns: [
@@ -73,6 +107,22 @@ const initialize_table1 = () => {
 
 const initialize_table2 = () => {
   var _table = $("#dom_jq_event2").DataTable({
+    columns: [
+      { data: "date" },
+      { data: "amount" },
+      { data: "currency" },
+      { data: "type" },
+      { data: "comment" },
+    ],
+    pageLength: pageLength,
+    lengthMenu: lengthMenu,
+    order: [[2, "asc"]],
+    destroy: true,
+  });
+};
+
+const initialize_table3 = () => {
+  var _table = $("#dom_jq_event3").DataTable({
     columns: [
       { data: "date" },
       { data: "amount" },
@@ -175,14 +225,62 @@ const render_table2 = () => {
   });
 };
 
+const render_table3 = () => {
+  document.querySelector("#dom_jq_event3").remove();
+  create_table3_layout();
+  var tbody = document.querySelector("#dom_jq_event3 tbody");
+  if ($.fn.dataTable.isDataTable("#dom_jq_event3")) {
+    $("#dom_jq_event3").DataTable().destroy();
+  }
+  let iterator = 0;
+  com_transactions.forEach((transaction) => {
+    //currency controller
+    let current_curr = String(currencies[selected_cur]);
+    let obj_price = "equalIn".concat(current_curr);
+    let options = { year: "numeric", month: "short", day: "numeric" };
+    var date = moment(transaction["date"], "DD-MM-YYYY")
+      .toDate()
+      .toLocaleDateString("en-GB", options);
+    var amount = transaction["amount"];
+    var currency = transaction["currency"];
+    var comment = transaction["comment"];
+    var type = transaction["type"];
+
+    let item =
+      `<tr id="asset-tr-` +
+      iterator +
+      `">
+      <td>` +
+      date +
+      `</td>
+      <td>` +
+      amount +
+      `</td>
+      <td>` +
+      currency +
+      `</td>
+      <td>` +
+      type +
+      `</td>
+      <td class="overflow-hidden text-wrap">` +
+      comment +
+      `</td>
+  </tr>`;
+    tbody.innerHTML += item;
+    iterator += 1;
+  });
+};
+
 const initialize_tables = () => {
   initialize_table1();
   initialize_table2();
+  initialize_table3();
 };
 
 const render_tables = () => {
   render_table1();
   render_table2();
+  render_table3();
 };
 
 //function to switch currencies
