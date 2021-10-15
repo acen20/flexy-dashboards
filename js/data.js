bd0d409841e828cd5499ee0120142817598b2318
acen20 <ahsen1330@gmail.com>
2021-10-15 21:15:36 +0500

    charts done

--- js/data.js
+++ js/data.js
@@ -1,45 +1,419 @@
 //commissions
-var all_commissions =
-{
-  "total":{
-    "EUR":"46.2300",
-    "USD":"54.3961",
-    "RUB":"3986.4291"
-  }
-}
+var all_commissions = {
+  total: {
+    EUR: "46.2300",
+    USD: "54.3961",
+    RUB: "3986.4291",
+  },
+};
 
 //deposits
 var all_top_ups = {
-  "total":{
-    "EUR":"1213.5500",
-    "USD":"1429.6065",
-    "RUB":"105038.2125"}
-}
+  total: {
+    EUR: "1213.5500",
+    USD: "1429.6065",
+    RUB: "105038.2125",
+  },
+};
 
 //Profitability
 var profitability = {
-  "total":{
-    "EUR":"5500",
-    "USD":"-6065",
-    "RUB":"2125"
+  total: {
+    EUR: "5500",
+    USD: "-6065",
+    RUB: "2125",
   },
-  "Monthly":{
-    "EUR":"500",
-    "USD":"-65",
-    "RUB":"125"
+  Monthly: {
+    EUR: "500",
+    USD: "-65",
+    RUB: "125",
   },
-  "Day":{
-    "EUR":"12",
-    "USD":"142",
-    "RUB":"105"
-  }
-}
+  Day: {
+    EUR: "12",
+    USD: "142",
+    RUB: "105",
+  },
+};
 
 //total_product
 var total_product = {
-  "total": 26
-}
+  total: 26,
+};
 
 var portfolioTotal = portfolioValue["total"];
 
-var deposits = {"total":{"EUR":"1213.5500","USD":"1429.6065","RUB":"105038.2125"},"transactions":[{"type":"Карточный платеж","date":"28-06-2021","account":" торговый ","amount":30,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"72.1694","RUB_USD":"0.01386","EUR_RUB":"86.1919","RUB_EUR":"0.01160","EUR_USD":"1.191","USD_EUR":"0.8396"},"equalInEUR":30,"equalInUSD":"35.7300","equalInRUB":"2585.7570"},{"type":"Карточный платеж","date":"14-07-2021","account":" торговый ","amount":40,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"74.0589","RUB_USD":"0.01350","EUR_RUB":"87.7820","RUB_EUR":"0.01139","EUR_USD":"1.1812","USD_EUR":"0.8466"},"equalInEUR":40,"equalInUSD":"47.2480","equalInRUB":"3511.2800"},{"type":"Карточный платеж","date":"15-07-2021","account":" торговый ","amount":85,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"74.1236","RUB_USD":"0.01349","EUR_RUB":"87.3917","RUB_EUR":"0.01144","EUR_USD":"1.1809","USD_EUR":"0.8468"},"equalInEUR":85,"equalInUSD":"100.3765","equalInRUB":"7428.2945"},{"type":"Карточный платеж","date":"16-07-2021","account":" торговый ","amount":5,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"74.2197","RUB_USD":"0.01347","EUR_RUB":"87.8316","RUB_EUR":"0.01139","EUR_USD":"1.1802","USD_EUR":"0.8473"},"equalInEUR":5,"equalInUSD":"5.9010","equalInRUB":"439.1580"},{"type":"Карточный платеж","date":"21-07-2021","account":" торговый ","amount":30,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"74.4910","RUB_USD":"0.01342","EUR_RUB":"87.7727","RUB_EUR":"0.01139","EUR_USD":"1.1772","USD_EUR":"0.8495"},"equalInEUR":30,"equalInUSD":"35.3160","equalInRUB":"2633.1810"},{"type":"Карточный платеж","date":"22-07-2021","account":" торговый ","amount":25,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"74.4947","RUB_USD":"0.01342","EUR_RUB":"87.5983","RUB_EUR":"0.01142","EUR_USD":"1.1775","USD_EUR":"0.8493"},"equalInEUR":25,"equalInUSD":"29.4375","equalInRUB":"2189.9575"},{"type":"Карточный платеж","date":"22-07-2021","account":" торговый ","amount":25,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"74.4947","RUB_USD":"0.01342","EUR_RUB":"87.5983","RUB_EUR":"0.01142","EUR_USD":"1.1775","USD_EUR":"0.8493"},"equalInEUR":25,"equalInUSD":"29.4375","equalInRUB":"2189.9575"},{"type":"Карточный платеж","date":"31-07-2021","account":" торговый ","amount":50,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"73.1388","RUB_USD":"0.01367","EUR_RUB":"86.9913","RUB_EUR":"0.01150","EUR_USD":1.1867,"USD_EUR":0.842673},"equalInEUR":50,"equalInUSD":"59.3350","equalInRUB":"4349.5650"},{"type":"Карточный платеж","date":"31-07-2021","account":" торговый ","amount":20,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"73.1388","RUB_USD":"0.01367","EUR_RUB":"86.9913","RUB_EUR":"0.01150","EUR_USD":1.1867,"USD_EUR":0.842673},"equalInEUR":20,"equalInUSD":"23.7340","equalInRUB":"1739.8260"},{"type":"Карточный платеж","date":"09-08-2021","account":" торговый ","amount":70,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"73.1304","RUB_USD":"0.01367","EUR_RUB":"86.4621","RUB_EUR":"0.01157","EUR_USD":"1.1761","USD_EUR":"0.8503"},"equalInEUR":70,"equalInUSD":"82.3270","equalInRUB":"6052.3470"},{"type":"Карточный платеж","date":"09-08-2021","account":" торговый ","amount":90,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"73.1304","RUB_USD":"0.01367","EUR_RUB":"86.4621","RUB_EUR":"0.01157","EUR_USD":"1.1761","USD_EUR":"0.8503"},"equalInEUR":90,"equalInUSD":"105.8490","equalInRUB":"7781.5890"},{"type":"Карточный платеж","date":"11-08-2021","account":" торговый ","amount":45,"currency":"EUR","comment":" Deposit with a bank card ","transactionType":"MONEYDEPOSIT","rates":{"USD_RUB":"73.5962","RUB_USD":"0.01359","EUR_RUB":"86.3578","RUB_EUR":"0.01158","EUR_USD":"1.1718","USD_EUR":"0.8534"},"equalInEUR":45,"equalInUSD":"52.7310","equalInRUB":"3886.1010"}
+var deposits = {
+  total: {
+    EUR: "1213.5500",
+    USD: "1429.6065",
+    RUB: "105038.2125",
+  },
+  transactions: [
+    {
+      type: "Карточный платеж",
+      date: "28-06-2021",
+      account: " торговый ",
+      amount: 30,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "72.1694",
+        RUB_USD: "0.01386",
+        EUR_RUB: "86.1919",
+        RUB_EUR: "0.01160",
+        EUR_USD: "1.191",
+        USD_EUR: "0.8396",
+      },
+      equalInEUR: 30,
+      equalInUSD: "35.7300",
+      equalInRUB: "2585.7570",
+    },
+    {
+      type: "Карточный платеж",
+      date: "14-07-2021",
+      account: " торговый ",
+      amount: 40,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "74.0589",
+        RUB_USD: "0.01350",
+        EUR_RUB: "87.7820",
+        RUB_EUR: "0.01139",
+        EUR_USD: "1.1812",
+        USD_EUR: "0.8466",
+      },
+      equalInEUR: 40,
+      equalInUSD: "47.2480",
+      equalInRUB: "3511.2800",
+    },
+    {
+      type: "Карточный платеж",
+      date: "15-07-2021",
+      account: " торговый ",
+      amount: 85,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "74.1236",
+        RUB_USD: "0.01349",
+        EUR_RUB: "87.3917",
+        RUB_EUR: "0.01144",
+        EUR_USD: "1.1809",
+        USD_EUR: "0.8468",
+      },
+      equalInEUR: 85,
+      equalInUSD: "100.3765",
+      equalInRUB: "7428.2945",
+    },
+    {
+      type: "Карточный платеж",
+      date: "16-07-2021",
+      account: " торговый ",
+      amount: 5,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "74.2197",
+        RUB_USD: "0.01347",
+        EUR_RUB: "87.8316",
+        RUB_EUR: "0.01139",
+        EUR_USD: "1.1802",
+        USD_EUR: "0.8473",
+      },
+      equalInEUR: 5,
+      equalInUSD: "5.9010",
+      equalInRUB: "439.1580",
+    },
+    {
+      type: "Карточный платеж",
+      date: "21-07-2021",
+      account: " торговый ",
+      amount: 30,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "74.4910",
+        RUB_USD: "0.01342",
+        EUR_RUB: "87.7727",
+        RUB_EUR: "0.01139",
+        EUR_USD: "1.1772",
+        USD_EUR: "0.8495",
+      },
+      equalInEUR: 30,
+      equalInUSD: "35.3160",
+      equalInRUB: "2633.1810",
+    },
+    {
+      type: "Карточный платеж",
+      date: "22-07-2021",
+      account: " торговый ",
+      amount: 25,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "74.4947",
+        RUB_USD: "0.01342",
+        EUR_RUB: "87.5983",
+        RUB_EUR: "0.01142",
+        EUR_USD: "1.1775",
+        USD_EUR: "0.8493",
+      },
+      equalInEUR: 25,
+      equalInUSD: "29.4375",
+      equalInRUB: "2189.9575",
+    },
+    {
+      type: "Карточный платеж",
+      date: "22-07-2021",
+      account: " торговый ",
+      amount: 25,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "74.4947",
+        RUB_USD: "0.01342",
+        EUR_RUB: "87.5983",
+        RUB_EUR: "0.01142",
+        EUR_USD: "1.1775",
+        USD_EUR: "0.8493",
+      },
+      equalInEUR: 25,
+      equalInUSD: "29.4375",
+      equalInRUB: "2189.9575",
+    },
+    {
+      type: "Карточный платеж",
+      date: "31-07-2021",
+      account: " торговый ",
+      amount: 50,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "73.1388",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.9913",
+        RUB_EUR: "0.01150",
+        EUR_USD: 1.1867,
+        USD_EUR: 0.842673,
+      },
+      equalInEUR: 50,
+      equalInUSD: "59.3350",
+      equalInRUB: "4349.5650",
+    },
+    {
+      type: "Карточный платеж",
+      date: "31-07-2021",
+      account: " торговый ",
+      amount: 20,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "73.1388",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.9913",
+        RUB_EUR: "0.01150",
+        EUR_USD: 1.1867,
+        USD_EUR: 0.842673,
+      },
+      equalInEUR: 20,
+      equalInUSD: "23.7340",
+      equalInRUB: "1739.8260",
+    },
+    {
+      type: "Карточный платеж",
+      date: "09-08-2021",
+      account: " торговый ",
+      amount: 70,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "73.1304",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.4621",
+        RUB_EUR: "0.01157",
+        EUR_USD: "1.1761",
+        USD_EUR: "0.8503",
+      },
+      equalInEUR: 70,
+      equalInUSD: "82.3270",
+      equalInRUB: "6052.3470",
+    },
+    {
+      type: "Карточный платеж",
+      date: "09-08-2021",
+      account: " торговый ",
+      amount: 90,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "73.1304",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.4621",
+        RUB_EUR: "0.01157",
+        EUR_USD: "1.1761",
+        USD_EUR: "0.8503",
+      },
+      equalInEUR: 90,
+      equalInUSD: "105.8490",
+      equalInRUB: "7781.5890",
+    },
+    {
+      type: "Карточный платеж",
+      date: "11-08-2021",
+      account: " торговый ",
+      amount: 45,
+      currency: "EUR",
+      comment: " Deposit with a bank card ",
+      transactionType: "MONEYDEPOSIT",
+      rates: {
+        USD_RUB: "73.5962",
+        RUB_USD: "0.01359",
+        EUR_RUB: "86.3578",
+        RUB_EUR: "0.01158",
+        EUR_USD: "1.1718",
+        USD_EUR: "0.8534",
+      },
+      equalInEUR: 45,
+      equalInUSD: "52.7310",
+      equalInRUB: "3886.1010",
+    },
+  ],
+};
+
+var commissions = {
+  total: { EUR: "46.2300", USD: "54.3961", RUB: "3986.4291" },
+  transactions: [
+    {
+      type: " За сделку:  159750750",
+      date: "22-07-2021",
+      amount: 0.2,
+      currency: "EUR",
+      comment:
+        " Market: russia, security type: stocks, commission currency: EUR, service plan: Smart in EUR, trade currency: RUR.  The minimum commission per cent of the trade amount is set to: 0.08.  Currency exchange of trade (RUR) to a currency exchange of the service plan (EUR) is 0.0114.  Minimum commission for trade in the tariff currency 0.2: EUR is set. ",
+      transactionType: "COMMISSION",
+      rates: {
+        USD_RUB: "74.4947",
+        RUB_USD: "0.01342",
+        EUR_RUB: "87.5983",
+        RUB_EUR: "0.01142",
+        EUR_USD: "1.1775",
+        USD_EUR: "0.8493",
+      },
+      equalInEUR: 0.2,
+      equalInUSD: "0.2355",
+      equalInRUB: "17.5197",
+    },
+    {
+      type: " За сделку:  159805555",
+      date: "22-07-2021",
+      amount: 0.2,
+      currency: "EUR",
+      comment:
+        " Market: russia, security type: stocks, commission currency: EUR, service plan: Smart in EUR, trade currency: RUR.  The minimum commission per cent of the trade amount is set to: 0.08.  Currency exchange of trade (RUR) to a currency exchange of the service plan (EUR) is 0.0114.  Minimum commission for trade in the tariff currency 0.2: EUR is set. ",
+      transactionType: "COMMISSION",
+      rates: {
+        USD_RUB: "74.4947",
+        RUB_USD: "0.01342",
+        EUR_RUB: "87.5983",
+        RUB_EUR: "0.01142",
+        EUR_USD: "1.1775",
+        USD_EUR: "0.8493",
+      },
+      equalInEUR: 0.2,
+      equalInUSD: "0.2355",
+      equalInRUB: "17.5197",
+    },
+    {
+      type: " За сделку:  163057076",
+      date: "02-08-2021",
+      amount: 0.2,
+      currency: "EUR",
+      comment:
+        " Market: russia, security type: bonds, commission currency: EUR, service plan: Smart in EUR, trade currency: RUR.  Currency exchange of trade (RUR) to a currency exchange of the service plan (EUR) is 0.0115.  Additional commissions for a trade have been added:  Minimum per trade: 0.2 RUR.  Total additional commissions: 0.2 EUR ",
+      transactionType: "COMMISSION",
+      rates: {
+        USD_RUB: "73.1388",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.9913",
+        RUB_EUR: "0.01150",
+        EUR_USD: "1.1886",
+        USD_EUR: "0.8413",
+      },
+      equalInEUR: 0.2,
+      equalInUSD: "0.2377",
+      equalInRUB: "17.3983",
+    },
+    {
+      type: " За сделку:  163057079",
+      date: "02-08-2021",
+      amount: 0.2,
+      currency: "EUR",
+      comment:
+        " Market: russia, security type: stocks, commission currency: EUR, service plan: Smart in EUR, trade currency: RUR.  The minimum commission per cent of the trade amount is set to: 0.08.  Currency exchange of trade (RUR) to a currency exchange of the service plan (EUR) is 0.0115.  Minimum commission for trade in the tariff currency 0.2: EUR is set. ",
+      transactionType: "COMMISSION",
+      rates: {
+        USD_RUB: "73.1388",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.9913",
+        RUB_EUR: "0.01150",
+        EUR_USD: "1.1886",
+        USD_EUR: "0.8413",
+      },
+      equalInEUR: 0.2,
+      equalInUSD: "0.2377",
+      equalInRUB: "17.3983",
+    },
+    {
+      type: " За сделку:  165081000",
+      date: "09-08-2021",
+      amount: 0.2,
+      currency: "EUR",
+      comment:
+        " Market: russia, security type: stocks, commission currency: EUR, service plan: Smart in EUR, trade currency: RUR.  The minimum commission per cent of the trade amount is set to: 0.08.  Currency exchange of trade (RUR) to a currency exchange of the service plan (EUR) is 0.0116.  Minimum commission for trade in the tariff currency 0.2: EUR is set. ",
+      transactionType: "COMMISSION",
+      rates: {
+        USD_RUB: "73.1304",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.4621",
+        RUB_EUR: "0.01157",
+        EUR_USD: "1.1761",
+        USD_EUR: "0.8503",
+      },
+      equalInEUR: 0.2,
+      equalInUSD: "0.2352",
+      equalInRUB: "17.2924",
+    },
+    {
+      type: " За сделку:  165080865",
+      date: "09-08-2021",
+      amount: 0.2,
+      currency: "EUR",
+      comment:
+        " Market: russia, security type: stocks, commission currency: EUR, service plan: Smart in EUR, trade currency: RUR.  The minimum commission per cent of the trade amount is set to: 0.08.  Currency exchange of trade (RUR) to a currency exchange of the service plan (EUR) is 0.0116.  Minimum commission for trade in the tariff currency 0.2: EUR is set. ",
+      transactionType: "COMMISSION",
+      rates: {
+        USD_RUB: "73.1304",
+        RUB_USD: "0.01367",
+        EUR_RUB: "86.4621",
+        RUB_EUR: "0.01157",
+        EUR_USD: "1.1761",
+        USD_EUR: "0.8503",
+      },
+      equalInEUR: 0.2,
+      equalInUSD: "0.2352",
+      equalInRUB: "17.2924",
+    },
+  ],
+};
--- js/transactions.js
+++ js/transactions.js
@@ -1,13 +1,40 @@
-var assets = portfolioValue["assets"];
+var transactions = deposits["transactions"];
+var com_transactions = commissions["transactions"];
+var pageLength = 5;
 
-function create_table1_layout(){
+function create_table1_layout() {
   var table_layout = `  <table id="dom_jq_event1" class="w-100 table table-striped table-bordered no-wrap">
       <thead>
           <tr>
-          <th>Ticker</th>
-          <th>Portfolio %</th>
+          <th>Date</th>
           <th>Amount</th>
-          <th>Current Price</th>
+          <th>Currency</th>
+          <th>Comment</th>
+          </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      <tfoot>
+          <tr>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Currency</th>
+          <th>Comment</th>
+          </tr>
+      </tfoot>
+  </table>`;
+  document.querySelector("#table1-container").innerHTML = table_layout;
+}
+
+function create_table2_layout() {
+  var table_layout = `  <table id="dom_jq_event2" class="w-100 table table-striped table-bordered no-wrap">
+      <thead>
+          <tr>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Currency</th>
+          <th>Comment</th>
+          <th>Comment</th>
           </tr>
       </thead>
       <tbody>
@@ -18,67 +45,144 @@ function create_table1_layout(){
             <th>Portfolio %</th>
             <th>Amount</th>
             <th>Current Price</th>
+            <th>Comment</th>
           </tr>
       </tfoot>
-  </table>`
-  document.querySelector('#table1-container').innerHTML = table_layout;
+  </table>`;
+  document.querySelector("#table2-container").innerHTML = table_layout;
 }
 
-const initialize_table1 = () =>{
+const initialize_table1 = () => {
   var _table = $("#dom_jq_event1").DataTable({
-		"columns": [
-		    { "data": "date"},
-		    { "data": "amount" },
-		    { "data": "currency" },
-		    { "data": "comment" },
-		],
-		"order": [[2, 'asc']],
-    destroy: true
-	});
-}
+    columns: [
+      { data: "date" },
+      { data: "amount" },
+      { data: "currency" },
+      { data: "comment" },
+    ],
+    pageLength: 5,
+    order: [[2, "asc"]],
+    destroy: true,
+  });
+};
+
+const initialize_table2 = () => {
+  var _table = $("#dom_jq_event2").DataTable({
+    columns: [
+      { data: "date" },
+      { data: "amount" },
+      { data: "currency" },
+      { data: "comment" },
+      { data: "comment" },
+    ],
+    pageLength: pageLength,
+    order: [[2, "asc"]],
+    destroy: true,
+  });
+};
 
 const render_table1 = () => {
-  document.querySelector('#dom_jq_event1').remove();
+  document.querySelector("#dom_jq_event1").remove();
   create_table1_layout();
-  var tbody = document.querySelector('tbody');
-  if ($.fn.dataTable.isDataTable('#dom_jq_event1')) {
-        $('#dom_jq_event1').DataTable().destroy();
-    }
+  var tbody = document.querySelector("tbody");
+  if ($.fn.dataTable.isDataTable("#dom_jq_event1")) {
+    $("#dom_jq_event1").DataTable().destroy();
+  }
   let iterator = 0;
-  assets.forEach((asset) => {
+  transactions.forEach((transaction) => {
+    //currency controller
+    let current_curr = String(currencies[selected_cur]);
+    let obj_price = "equalIn".concat(current_curr);
+    let options = { year: "numeric", month: "short", day: "numeric" };
+    var date = moment(transaction["date"], "DD-MM-YYYY")
+      .toDate()
+      .toLocaleDateString("en-GB", options);
+    var amount = transaction["amount"];
+    var currency = transaction["currency"];
+    var comment = transaction["comment"];
 
-  //currency controller
-  let current_curr = String(currencies[selected_cur]);
-  let obj_price = 'equalIn'.concat(current_curr);
-
-  var date = asset['date']
-  var amount = asset['amount'];
-  var currency = asset['currency'];
-
-  let item = `<tr id="asset-tr-`+iterator+`">
-      <td>`+ticker+`</td>
-      <td>`+portfolioPercent+`</td>
-      <td>`+amount+`</td>
-      <td>`+type+`</td>
+    let item =
+      `<tr id="asset-tr-` +
+      iterator +
+      `">
+      <td>` +
+      date +
+      `</td>
+      <td>` +
+      amount +
+      `</td>
+      <td>` +
+      currency +
+      `</td>
+      <td>` +
+      comment +
+      `</td>
   </tr>`;
-  tbody.innerHTML += item;
-  iterator += 1;
-});
-}
+    tbody.innerHTML += item;
+    iterator += 1;
+  });
+};
 
-const initialize_tables = () =>{
+const render_table2 = () => {
+  document.querySelector("#dom_jq_event2").remove();
+  create_table2_layout();
+  var tbody = document.querySelector("tbody");
+  if ($.fn.dataTable.isDataTable("#dom_jq_event1")) {
+    $("#dom_jq_event1").DataTable().destroy();
+  }
+  let iterator = 0;
+  transactions.forEach((transaction) => {
+    //currency controller
+    let current_curr = String(currencies[selected_cur]);
+    let obj_price = "equalIn".concat(current_curr);
+    let options = { year: "numeric", month: "short", day: "numeric" };
+    var date = moment(transaction["date"], "DD-MM-YYYY")
+      .toDate()
+      .toLocaleDateString("en-GB", options);
+    var amount = transaction["amount"];
+    var currency = transaction["currency"];
+    var comment = transaction["comment"];
+
+    let item =
+      `<tr id="asset-tr-` +
+      iterator +
+      `">
+      <td>` +
+      date +
+      `</td>
+      <td>` +
+      amount +
+      `</td>
+      <td>` +
+      currency +
+      `</td>
+      <td>` +
+      type +
+      `</td>
+      <td>` +
+      comment +
+      `</td>
+  </tr>`;
+    tbody.innerHTML += item;
+    iterator += 1;
+  });
+};
+
+const initialize_tables = () => {
   initialize_table1();
-}
+  initialize_table2();
+};
 
-const render_tables = () =>{
+const render_tables = () => {
   render_table1();
-}
+  render_table2();
+};
 
 //function to switch currencies
 
 const change_asset_currency = () => {
   render_tables();
   initialize_tables();
-}
+};
 
 change_asset_currency();
